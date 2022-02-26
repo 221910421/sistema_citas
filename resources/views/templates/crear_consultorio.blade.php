0a6bc178e8399b26c6404cc09ec68e85b095e521
Jessica Ivonne Gil Gomez <al221910315@gmail.com>
2022-02-25 22:33:12 -0600

    crear consultorio

---
+++ resources/views/templates/crear_consultorio.blade.php
@@ -0,0 +1,16 @@
+@extends('templates-layouts.headerandfooter')
+@section('body')
+<h1>Ingresa los datos del formulario para registrarte</h1>
+<form action="{{route('guardar_consultorio')}}" method="post" enctype="multipart/form-data">
+  @csrf
+  <label>Numero de consultorio:</label><br>
+  <input type="number" name="numero_de_consultorio"><br><br>
+  <label>id Especialidad:</label><br>
+  <input type="number" name="id_especialidad"><br><br>
+  <label>id Estatus:</label><br>
+  <input type="number" name="id_estatus"><br><br>
+  <input type="submit" value="Crear consultorio">
+</form>
+
+
+@endsection
