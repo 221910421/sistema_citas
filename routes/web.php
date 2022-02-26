4688c38ea767944bc8357af30d557740b749aa04
Jessica Ivonne Gil Gomez <al221910315@gmail.com>
2022-02-25 22:19:11 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('templates.index');
 });
+
+///---------------iniciar sesion--------------
+Route::get('iniciarsesion/', function () {
+    return view('templates.iniciar_sesion');
+});
+Route::name('login')->post('login/', 'App\Http\Controllers\loginController@validar');
+Route::name('salir')->get('salir/', 'App\Http\Controllers\loginController@logout');
