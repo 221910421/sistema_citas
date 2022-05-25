b8bcf196a8e26be2f29ea9bedd1c5ebc610a4b43
Miguel Betzay Dominguez Diaz <al221910421@gmail.com>
2022-05-25 13:18:19 -0500

    Restringir permisos en todas las tablas 1

--- resources/views/templates/citas/crear_cita.blade.php
+++ resources/views/templates/citas/crear_cita.blade.php
@@ -29,6 +29,14 @@
 <div class="appointment py-5">
     <div class="py-xl-5 py-lg-3">
         <div class="w3ls-titles text-center mb-5">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function () {
+        var refreshId = setInterval(function () {
+            $('#sesion').load("{{route('verificar_sesion')}}");
+        }, 500);
+    });
+</script>
             <h3 class="title">Crear mi cita {{session('session_name')}} {{session('session_ap')}}</h3>
             <span>
                 <i class="fas fa-user-md"></i>
@@ -147,14 +155,6 @@
                 $('.events').append("<li>after event fired.</li>");
             }
         });
-    });
-</script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>
-    $(document).ready(function () {
-        var refreshId = setInterval(function () {
-            $('#sesion').load("{{route('verificar_sesion')}}");
-        }, 500);
     });
 </script>
 <!-- //banner slider -->
