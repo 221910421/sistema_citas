fc5f0857ccbe36363acac4fd34903c173ab2400b
Miguel Betzay Dominguez <al221910421@gmail.com>
2022-07-04 21:21:01 -0500

    Arreglo de crear citas envia al inicio

--- resources/views/templates/citas/crear_cita.blade.php
+++ resources/views/templates/citas/crear_cita.blade.php
@@ -29,14 +29,6 @@
 <div class="appointment py-5">
     <div class="py-xl-5 py-lg-3">
         <div class="w3ls-titles text-center mb-5">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script>
-            $(document).ready(function () {
-            var refreshId = setInterval(function () {
-            window.location.href="/";
-            }, 500);
-            });
-        </script>
             <h3 class="title">Crear mi cita {{session('session_name')}} {{session('session_ap')}}</h3>
             <span>
                 <i class="fas fa-user-md"></i>
