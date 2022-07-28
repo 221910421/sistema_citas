43d85713da3213b8ecf5462b7b5ec9523a2d88fa
Miguel Betzay Dominguez <al221910421@gmail.com>
2022-07-27 22:35:33 -0500

    Arreglo de clase crypt

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use \Crypt;
 use App\Models\pacientes;
+use Illuminate\Support\Facades\Crypt;
 
 class LoginController extends Controller
 {
