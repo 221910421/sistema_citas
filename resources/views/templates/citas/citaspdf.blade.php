b3ae192ca023a919c8ea0c82cb38456307c76276
Jessica Ivonne Gil Gomez <al221910315@gmail.com>
2022-08-10 23:04:58 -0500

    pdf

--- resources/views/templates/citas/citaspdf.blade.php
+++ resources/views/templates/citas/citaspdf.blade.php
@@ -1,17 +1,21 @@
-
-<h1>Reporte de citas</h1>
-<form action="" method="get">
-    <label for="">Elija el campo del que desea tomar la información:</label>
-    <select name="campo" id="campo">
-        <option value="">Elija una opción</option>
-        <option value="fecha_cita">Fecha</option>
-        <option value="hora_cita">Hora</option>
-        <option value="folio">Folio</option>
-    </select>
-    <br>
-    <label for="">Buscar: </label>
-    <input type="text" name="buscar" id="buscar" placeholder="Termino a buscar" onkeyup="comprobarHora()">
-</form>
+<style>
+    .titulo{
+      text-align: center;
+      color: blue;
+    }
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    #container{
+      background-color: rgb(251, 245, 245);
+      width: 200px;
+      height: 90px;
+      margin-left:auto; margin-right:0;
+    }
+  </style>
+<center><h1>Reporte de citas</h1></center>
 <div id="table-content" class="table-responsive">
     <table class="table">
         <thead>
@@ -31,9 +35,6 @@
                 <th>
                     <h3>Estatus</h3>
                 </th>
-                <th>
-                    <h3>opciones</h3>
-                </th>
             </tr>
         </thead>
         <tbody>
@@ -44,36 +45,9 @@
                 <td>{{$cita->fecha_cita}}</td>
                 <td>{{$cita->hora_cita}}</td>
                 <td>{{$cita->estatus_cita}}</td>
-                <td>
-                    <form action="{{route('detalles_cita')}}" method="post">
-                        @csrf
-                        <input type="text" name="id" readonly value="{{$cita->id_cita}}" hidden>
-                        <input type="text" name="paciente" readonly value="{{$cita->id_paciente}}" hidden>
-                        <input type="text" name="folio" readonly value="{{$cita->folio}}" hidden>
-                        <input type="submit"
-                            style="border-radius: 5px; width: 130px; cursor: pointer; background-color: aqua;"
-                            value="Ver detalles">
-                    </form>
-                    <form id="cancelar" action="{{route('cancelar_cita')}}" method="post">
-                        @csrf
-                        <input type="text" name="id" readonly value="{{$cita->id_cita}}" hidden>
-                        <input type="text" name="folio" readonly value="{{$cita->folio}}" hidden>
-                        @if($cita->estatus_cita != "Cancelado")
-                        <input type="submit" value="Cancelar cita"
-                            style="border-radius: 5px; width: 130px; cursor: pointer; background-color: red;">
-                            
-                        @else
-                        <input type="button" value="Cancelar cita"
-                            style="border-radius: 5px; width: 130px; cursor: pointer; background-color: red;" onclick="cancelado()">
-                        @endif
-                    </form>
-                </td>
             </tr>
             @endforeach
         </tbody>
     </table>
 </div>
 
-<div id="sesion">
-
-</div>
